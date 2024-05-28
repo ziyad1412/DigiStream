8dbd649bae2802ded33987b2eef03d0a40a180f2
Abdurrahman Ziyad <abdurrahman.ziyad17@gmail.com>
2024-05-28 21:07:03 +0700

    feat:Setup Breeze + Spite

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admin = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('admin123'),
+        ]);
+        $admin->assignRole('admin');
+    }
+}
