04bf16c840bed4db3440b379e586611d32eb246f
Abdurrahman Ziyad <abdurrahman.ziyad17@gmail.com>
2024-05-31 16:14:45 +0700

    Migration and Seeder

---
+++ app/Http/Controllers/UserSubscriptionController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserSubscription;
+use Illuminate\Http\Request;
+
+class UserSubscriptionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(UserSubscription $userSubscription)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(UserSubscription $userSubscription)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, UserSubscription $userSubscription)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(UserSubscription $userSubscription)
+    {
+        //
+    }
+}
